d60205e622432f5b621c4d9638266b4d352db0f1
kenielf <61657416+kenielf@users.noreply.github.com>
2021-12-12 00:43:28 -0300

    Isolated Windows and Linux Interfaces smartly

--- source/statcalc.py
+++ source/statcalc.py
@@ -1,23 +1,29 @@
 #!/bin/env python
 import sys
-from interface import *
+import platform
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
+    # Check OS
+    if platform.system() == "Linux":
+        import interface_lin as ui
+    elif platform.system() == "Windows":
+        import interface_win as ui
+    # Run
     if "-t" in args or "--terminal" in args:
         print("Running as CLI")
-        interface = Terminal()
+        interface = ui.Terminal()
         interface.run()
     elif "-g" in args or "--graphical" in args:
         print("Running as GUI")
-        interface = Graphical()
+        interface = ui.Graphical()
         interface.run()
     elif "-h" in args or "--help" in args:
-        help_print()
+        ui.help_print()
     elif len(args) == 0:
         print("No interface specified, defaulting to Graphical")
-        interface = Graphical()
+        interface = ui.Graphical()
         interface.run()
     else:
-        help_print()
+        ui.help_print()
